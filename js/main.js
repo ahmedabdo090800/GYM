a95fc085aeb97ffdee047c8272ae4ffcd1de2eb9
ahmedabdo090800 <ahmedabdo090800@gmail.com>
2023-06-26 14:25:44 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// navlist=document.getElementsByClassName("nav-list");
+// nav_list_show=document.getElementsByClassName("show");
+
+// function togle(){
+//     navlist.classList.add("show");
+// }
+
+$(".button-menu").click(function(){
+
+    $(".nav-list").addClass("show");
+    })
+
+ $("button").click(function(){
+        $("ul").toggle();
+});
